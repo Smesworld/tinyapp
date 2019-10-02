5f248f5dea1a6b4fb751a82f82b84763b5f3ad9d
Alan Dodge <alphaechododge@gmail.com>
2019-10-02 00:00:04 +0000

    Removed all instances of username, removed console.logs, linted

--- express_server.js
+++ express_server.js
@@ -10,18 +10,18 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 
-const users = { 
+const users = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
- "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
-}
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -59,7 +59,7 @@ app.post("/login", (req, res) => {
     res.cookie("user_id", userID);
     res.redirect('/urls');
   } else {
-    res.render('login', { status: 400, msg: "Invalid email/password"});
+    res.render('login', { status: 403, msg: "Invalid email/password"});
   }
 });
 
@@ -80,7 +80,7 @@ app.post("/register", (req, res) => {
   if (email === "" || password === "") {
     res.render('register', { status: 400, msg: "Enter non-empty email/password"});
   } else if (emailLookup(email)) {
-    res.render('register', { status: 400, msg: "That email is already in use"})
+    res.render('register', { status: 400, msg: "That email is already in use"});
   } else {
     users[userID] = {
       id: userID,
@@ -99,7 +99,7 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/urls/new", (req, res) => {
   if (req.cookies['user_id']) {
-    user = users[req.cookies['user_id']];
+    const user = users[req.cookies['user_id']];
     res.render("urls_new",  { user });
   } else {
     res.redirect('/login');
@@ -125,7 +125,6 @@ app.post("/urls/", (req, res) => {
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
-  console.log("DELETE", req.params.shortURL);
   delete urlDatabase[req.params.shortURL];
   res.redirect(`/urls`);
 });
@@ -137,7 +136,7 @@ app.post("/urls/:shortURL", (req, res) => {
 
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = {
-    user: users[req.cookies['username']],
+    user: users[req.cookies['user_id']],
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL]
   };
@@ -146,7 +145,6 @@ app.get("/urls/:shortURL", (req, res) => {
 
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
-  // console.log(longURL);
   res.redirect(longURL);
 });
 
